0e4480fcf9287ee0af9b5f703b30d97494c94c3a
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-07-10 16:03:13 -0300

    a

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 10
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}/* Condição Simples - aqui foi criado uma condição de IF(se) que da a entender "se sual vel for maior do que 60 então ira apresentar a mensagem de multado*/
+
+console.log(`Dirija sempre usando cinto de segurança`)
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}//Condição Composta - aqui foi criado uma condição de verdadeiro ou falso fazendo com que tenha dois resultados dependendo da var escolhida 
