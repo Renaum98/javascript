b2b816f7baa03399180b482ab8d433ead2d4e40c
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-07-12 11:32:03 -0300

    a

---
+++ aula12/testeif.js
@@ -0,0 +1,8 @@
+var cidade = 'Mooca'
+if (cidade == 'Santo André') {
+    console.log('Você mora em Santo André, em São Paulo')
+} else if (cidade == 'Mooca') {
+    console.log('Você mora na Mooca, em São Paulo')
+} else {
+    console.log('Não sei onde você mora')
+}
