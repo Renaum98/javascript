94d651080ebd1fa63f77b698205f07285b5e1a5e
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-07-18 16:04:10 -0300

    Testes

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*Estrutura de Peretição de Teste no Final*/
+var c = 1 // aqui o teste logico so roda depois do bloco com uma condição
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 20) 
+
+
+/* Estrutura de Repetição de teste no inicio
+var c = 1
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++
+}
+*/
+
+
