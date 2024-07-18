7bbd0e83b7b1fa7b6e688729b44d7014050708ed
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-07-18 16:36:57 -0300

    a

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,10 +1,16 @@
-/*Estrutura de Peretição de Teste no Final*/
+/*Estrutura de Repetição de Teste Variavel */
+for (var c=1; c<=6; c++){ /*com esse metodo o teste recebe um 'inicio, teste, incremento' que significa que sempre que o teste for verdadeiro com base no teste, ele ira repetir com o incremento, assim que o teste for falso então ele encerra o teste logico*/
+    console.log (`Esse é o Numero ${c}`)
+}
+
+/*Estrutura de Peretição de Teste no Final
+
 var c = 1 // aqui o teste logico so roda depois do bloco com uma condição
 do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 20) 
-
+*/
 
 /* Estrutura de Repetição de teste no inicio
 var c = 1
