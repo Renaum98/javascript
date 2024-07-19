6b100ea366d88a28ed5b94882de760b852ff42b0
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-07-19 08:28:33 -0300

    a

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -4,13 +4,13 @@
 /*Estrutura de Repetição de Teste Variavel 
 com esse metodo o teste recebe um 'inicio, teste, incremento' que significa que sempre que o teste for verdadeiro com base no teste, ele ira repetir com o incremento, assim que o teste for falso então ele encerra o teste logico*/
 
-/*for (var n1 = 1; n1==10; n1*n1){
-    console.log (`1 x ${n1}`)
-}*/
-
-for (var c=1; c<=6; c++){
-    console.log (`Esse é o Numero ${c}`)
+for (var n = 2; n<=10; n*n){
+    console.log (`2 x ${n}`)
 }
+
+/*for (var c=1; c<=6; c++){
+    console.log (`Esse é o Numero ${c}`)
+}*/
 
 /*Estrutura de Retição de Teste no Final
 
