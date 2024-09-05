9a6b66678c59643605a9357d956950bd6949f171
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-09-05 15:20:40 -0300

    as

--- exaula16/script.js
+++ exaula16/script.js
@@ -24,21 +24,45 @@ function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado`
+        item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         window.alert('Valor invalido ou ja encontrado na lista.')
     }
     num.value = ''
     num.focus()
 }
+
+
+
 function finalizar() {
     if (valores.length == 0) {
         window.alert('Adicione valores antes de finalizar')
     } else {
-        let tot = valores.length
-        
+        var total = valores.length
+        var maior = valores [0]
+        var menor = valores [0]
+        var soma = 0
+        var media = 0
+        for(var pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
         res.innerHTML = ''
-        res.innerHTML += `<p> Ao todo, temos ${tot} numeros cadastrados. </p>`
-    }
+        res.innerHTML += `<p>Ao todo, temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informador foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informador foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
+        }
+}
+function limpar() {
+    lista.innerHTML = ''
+    res.innerHTML = ''
+    valores = []
 }
