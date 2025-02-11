3d824717fc752e9751ea7fef95de63a96099ae4c
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-02-11 12:06:46 -0300

    qwe

---
+++ Novo Curso/aula01/js/index.js
@@ -0,0 +1,11 @@
+document.   
+    getElementById("texto").
+        innerHTML = 'Meu primeiro texto <b>JS</b>!';
+
+
+        
+alert(10+5);
+console.log('Ola, isso é um console.log');
+let a,b,c;
+a = 5; b = 6; c = 7;
+
