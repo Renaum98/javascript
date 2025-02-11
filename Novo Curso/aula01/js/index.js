6aa290971523c9529f26d7c51cb195e009852aa9
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-02-11 14:15:54 -0300

    Update index.js

--- Novo Curso/aula01/js/index.js
+++ Novo Curso/aula01/js/index.js
@@ -9,3 +9,10 @@ console.log('Ola, isso é um console.log');
 let a,b,c;
 a = 5; b = 6; c = 7;
 
+function soma(a,b){
+    return a + b;
+}
+
+document.
+    getElementById('texto').
+        innerHTML = soma(10,23);
